2aa963b7e818781fcab091f8def6bf32d5bcdd98
Kou <divaaripp@gmail.com>
2024-09-19 03:13:35 +0700

    Color changes

--- src/newb/config.h
+++ src/newb/config.h
@@ -1,45 +1,47 @@
 #ifndef NL_CONFIG_H
 #define NL_CONFIG_H
+// line 3 reserved
 
 /*
-  NEWB SHADER MAIN CONFIG
-  This part contains base configuration options for the shader.
+  EDITING CONFIG:
 
-  TOGGLES
-  - If [toggle] is mentioned, then
-    options can be commented to disable (by adding '//')
-  - eg: #define PLANTS_WAVE    -> this is ON
-        //#define PLANTS_WAVE  -> this is OFF
+  > TOGGLES
+  if [toggle] is mentioned, then
+  options can be commented to disable (by adding/removing '//')
+  eg: #define PLANTS_WAVE    -> this is ON
+      //#define PLANTS_WAVE  -> this is OFF
 
-  COLOR VALUES
-  - Color format: vec3(red,green,blue)
-  - 1.0 means 100%, 0.0 means 0%
-  - eg: vec3(1.0,1.0,0.0) = yellow
+  > COLOR VALUES
+  color format: vec3(<red>,<green>,<blue>)
+  1.0 means 100%, 0.0 means 0%
+  eg. vec3(1.0,1.0,0.0) = yellow
 
-  VALUES
-  - Values must be decimal
-  - eg. 32 is wrong, 32.0 is correct
+  > VALUES
+  values must be decimal
+  eg. 32 is wrong, 32.0 is correct
 
-  TYPES
-  - Should be integer. options to choose will be mentioned there
-  - eg: #define NL_FOG_TYPE 2
+  > TYPES
+  should be integer. options to choose will be mentioned there
+  eg: #define NL_FOG_TYPE 2
 
-  Remember to rebuild the shader after making changes.
+  BUILD THE SHADER AFTER EACH EDIT
 */
 
+/* -------- CONFIG STARTS HERE ----------- */
+
 /* Color correction */
-#define NL_TONEMAP_TYPE 3            // 1:Exponential, 2:Reinhard, 3:Extended Reinhard, 4:ACES
-#define NL_CONTRAST 0.75             // 0.3 low ~ 2.0 high
-//#define NL_EXPOSURE 1.3            // [toggle] 0.5 dark ~ 3.0 bright
-//#define NL_SATURATION 1.4          // [toggle] 0.0 grayscale ~ 4.0 super saturated
+#define NL_TONEMAP_TYPE 3   // 1:Exponential, 2:Reinhard, 3:Extended Reinhard, 4:ACES
+#define NL_CONSTRAST 0.75   // 0.3 low ~ 2.0 high
+//#define NL_EXPOSURE 1.3   // [toggle] 0.5 dark ~ 3.0 bright
+//#define NL_SATURATION 1.4 // [toggle] 0.0 grayscale ~ 4.0 super saturated
 //#define NL_TINT vec3(1.0,0.75,0.5) // [toggle] color overlay
 
 /* Terrain lighting */
-#define NL_SUN_INTENSITY 2.95   // 0.5 weak ~ 5.0 bright
-#define NL_TORCH_INTENSITY 1.0  // 0.5 weak ~ 3.0 bright
-#define NL_NIGHT_BRIGHTNESS 0.1 // 0.0 dark ~ 2.0 bright
-#define NL_CAVE_BRIGHTNESS 0.1  // 0.0 dark ~ 2.0 bright
-#define NL_SHADOW_INTENSITY 0.7 // 0.0 no shadow ~ 1.0 strong shadow
+#define NL_SUN_INTENSITY 4.0   // 0.5 weak ~ 5.0 bright
+#define NL_TORCH_INTENSITY 1.5  // 0.5 weak ~ 3.0 bright
+#define NL_NIGHT_BRIGHTNESS 0.0 // 0.0 dark ~ 2.0 bright
+#define NL_CAVE_BRIGHTNESS  0.1 // 0.0 dark ~ 2.0 bright
+#define NL_SHADOW_INTENSITY 0.8 // 0.0 no shadow ~ 1.0 strong shadow
 //#define NL_BLINKING_TORCH     // [toggle] flickering light
 //#define NL_CLOUD_SHADOW       // [toggle] cloud shadow (simple clouds only)
 
@@ -50,7 +52,7 @@
 
 /* Ambient light on terrain (light that is added everywhere) */
 #define NL_NETHER_AMBIENT vec3(3.0,2.16,1.89)
-#define NL_END_AMBIENT    vec3(1.98,1.25,2.3)
+#define NL_END_AMBIENT vec3(1.98,1.25,2.3)
 
 /* Torch colors */
 #define NL_OVERWORLD_TORCH_COL  vec3(1.0,0.52,0.18)
@@ -82,12 +84,12 @@
 #define NL_RAINBOW_RAIN 1.0  // 0.5 subtle ~ 2.0 bright during rain
 
 /* Ore glow intensity */
-#define NL_GLOW_TEX 2.2    // 0.4 weak ~ 8.0 bright
-#define NL_GLOW_SHIMMER    // [toggle] shimmer effect
+#define NL_GLOW_TEX 2.2  // 0.4 weak ~ 8.0 bright
+#define NL_GLOW_SHIMMER  // [toggle] shimmer effect
 //#define NL_GLOW_LEAK 0.6 // [toggle] 0.08 subtle ~ 1.0 100% brightness of NL_GLOW_TEX
 
 /* Waving */
-#define NL_PLANTS_WAVE 0.05    // [toggle] 0.02 gentle ~ 0.4 violent
+#define NL_PLANTS_WAVE 0.1   // [toggle] 0.02 gentle ~ 0.4 violent
 #define NL_LANTERN_WAVE 0.16   // [toggle] 0.05 subtle ~ 0.4 large swing
 #define NL_WAVE_SPEED 2.8      // 0.5 slow wave ~ 5.0 very fast wave
 //#define NL_EXTRA_PLANTS_WAVE // [toggle] !dont use! wave using texture coords (1.21.0 vanilla 2048x1024)
@@ -102,10 +104,10 @@
 #define NL_WATER_TINT vec3(0.52,0.9,0.45)
 
 /* Underwater */
-#define NL_UNDERWATER_BRIGHTNESS 0.8         // 0.0 dark ~ 3.0 bright
-#define NL_CAUSTIC_INTENSITY 1.9             // 0.5 weak ~ 5.0 bright
-#define NL_UNDERWATER_WAVE 0.1               // [toggle] 0.02 subtle ~ 0.6 trippy
-#define NL_UNDERWATER_STREAKS 1.0            // [toggle] 0.8 subtle - 2.0 bright streaks from top
+#define NL_UNDERWATER_BRIGHTNESS 0.8 // 0.0 dark ~ 3.0 bright
+#define NL_CAUSTIC_INTENSITY 1.9     // 0.5 weak ~ 5.0 bright
+#define NL_UNDERWATER_WAVE 0.1       // [toggle] 0.02 subtle ~ 0.6 trippy
+#define NL_UNDERWATER_STREAKS 1.0    // [toggle] 0.8 subtle - 2.0 bright streaks from top
 #define NL_UNDERWATER_TINT vec3(0.9,1.0,0.9) // fog tint color when underwater
 
 /* Cloud type */
@@ -115,7 +117,6 @@
 #define NL_CLOUD0_THICKNESS 2.1      // 0.5 slim ~ 8.0 fat
 #define NL_CLOUD0_RAIN_THICKNESS 4.0 // 0.5 slim ~ 8.0 fat
 #define NL_CLOUD0_OPACITY 0.9        // 0.0 invisible ~ 1.0 opaque
-#define NL_CLOUD0_MULTILAYER         // [toggle] extra cloud layer
 
 /* Soft cloud settings */
 #define NL_CLOUD1_SCALE vec2(0.016, 0.022) // 0.003 large ~ 0.2 tiny
@@ -131,7 +132,7 @@
 #define NL_CLOUD2_SCALE 0.033        // 0.003 large ~ 0.3 tiny
 #define NL_CLOUD2_SHAPE 0.5          // 0.0 round ~ 1.0 box
 #define NL_CLOUD2_DENSITY 25.0       // 1.0 blurry ~ 100.0 sharp
-#define NL_CLOUD2_VELOCITY 0.8       // 0.0 static ~ 4.0 very fast
+#define NL_CLOUD2_VELOCIY 0.8        // 0.0 static ~ 4.0 very fast
 //#define NL_CLOUD2_MULTILAYER       // [toggle] extra cloud layer
 
 /* Aurora settings */
@@ -142,11 +143,6 @@
 #define NL_AURORA_COL1 vec3(0.1,1.0,0.0)
 #define NL_AURORA_COL2 vec3(0.1,0.0,1.0)
 
-/* Shooting star */
-#define NL_SHOOTING_STAR 1.0        // [toggle] 0.2 dim ~ 1.0 bright
-#define NL_SHOOTING_STAR_PERIOD 6.0 // 0.4 fast ~ 12.0 slow (12 secs)
-#define NL_SHOOTING_STAR_DELAY 64.0  // 0.0 no delay ~ 100.0 long time (100 secs)
-
 /* Chunk loading slide in animation */
 //#define NL_CHUNK_LOAD_ANIM 100.0 // [toggle] -600.0 fall from top ~ 600.0 rise from bottom
 
@@ -155,26 +151,29 @@
 #define NL_SUNMOON_SIZE 1.0     // 0.3 tiny ~ 4.0 massive
 
 /* Fake godrays during sunrise/sunset */
-//#define NL_GODRAY 0.3 // [toggle] 0.1 subtle ~ 0.8 strong
+#define NL_GODRAY 0.3 // [toggle] 0.1 subtle ~ 0.8 strong
 
 /* Sky reflection */
 //#define NL_GROUND_REFL 0.4       // [toggle] 0.2 slightly reflective ~ 1.0 fully reflect sky 
 #define NL_GROUND_RAIN_WETNESS 1.0 // 0.0 no wetness ~ 1.0 fully wet blocks when raining
-#define NL_GROUND_RAIN_PUDDLES 0.7 // 0.0 no puddles ~ 1.0 puddles
+#define NL_GROUND_RAIN_PUDDLES 1.5 // 0.0 no puddles ~ 1.0 puddles
 //#define NL_GROUND_AURORA_REFL    // [toggle] aurora reflection on ground
+
+/* -------- CONFIG ENDS HERE ----------- */
 
 
 /*
-  NEWB SHADER SUBPACK CONFIG
-  This part contains custom configuration options for each subpack.
-
+  EDITING CONFIG FOR SUBPACKS:
+  
   If a value is already defined,
   then you must undefine it before modifying:
   eg: #undef OPTION_NAME
 
-  Subpack names and flags are inside `pack_config.toml`.
-  Build tool will enable corresponding flags when compiling. 
+  subpack names and flags are inside pack_config.sh.
+  pack.sh will enable corresponding flags when compiling. 
 */
+
+/* ------ SUBPACK CONFIG STARTS HERE -------- */
 
 #ifdef NO_WAVE_NO_FOG
   #define NO_WAVE
@@ -207,5 +206,7 @@
   #undef NL_CLOUD_TYPE
   #define NL_CLOUD_TYPE 0
 #endif
+
+/* ------ SUBPACK CONFIG ENDS HERE -------- */
 
 #endif
